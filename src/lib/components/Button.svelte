ce0c71abd80685b74de736be0b329f16f2a7e2d4
notRadioOpaque <ismailmuyideen.c05@gmail.com>
2025-03-24 08:40:23 +0100

    :rocket: button component done

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,10 @@
+<script>
+	let { action, label } = $props();
+</script>
+
+<button
+	onclick={action}
+	class="bg-btn-bg hover:text-primary-text hover:bg-btn-hover-bg rounded-full px-6 py-3 font-medium text-white transition-colors"
+>
+	{label}
+</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+	import { Button } from '$lib/components';
 	import { Dot } from '$lib/components';
 </script>
 
 <h1 class="!text-primary-text text-2xl">Welcome to SvelteKit</h1>
 
 <Dot size="sm" />
+
+<Button action={() => console.log('clicked')} label="Contact me"></Button>
