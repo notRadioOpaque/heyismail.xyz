0eceaf8b183f8fb7d5ebc2452b5af7a8c76be783
notRadioOpaque <ismailmuyideen.c05@gmail.com>
2025-03-24 08:19:30 +0100

    :rocket: Dot component done!

---
+++ src/lib/components/Dot.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	// Define the size prop with a default value of "md"
+	let { size = 'md' }: { size: 'sm' | 'md' } = $props();
+
+	// Define size mappings for the different options
+	const sizeMap = {
+		sm: {
+			orb: 'w-2 h-2',
+			glow: 'w-3 h-3'
+		},
+		md: {
+			orb: 'w-3 h-3',
+			glow: 'w-4 h-4'
+		}
+	};
+
+	// Get the correct size classes based on the prop
+	const orbSize = $derived(sizeMap[size]?.orb);
+	const glowSize = $derived(sizeMap[size]?.glow);
+</script>
+
+<div class="relative flex w-max items-center justify-center border bg-black">
+	<!-- The main orb with pulsating animation -->
+	<div class="{glowSize} bg-tertiary-bg animate-ping rounded-full"></div>
+
+	<div
+		class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 {orbSize} bg-tertiary-bg rounded-full"
+	></div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,7 @@
+<script lang="ts">
+	import { Dot } from '$lib/components';
+</script>
+
 <h1 class="!text-primary-text text-2xl">Welcome to SvelteKit</h1>
+
+<Dot size="sm" />
