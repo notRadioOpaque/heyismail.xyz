c90c1bf757e9a0918906249a0c8b27fd279b1e5c
notRadioOpaque <ismailmuyideen.c05@gmail.com>
2025-04-06 10:27:44 +0100

    :sparkles: added ExperienceCard component and integrated it into the experience page for displaying work experiences

---
+++ src/lib/components/ExperienceCard.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	interface Props {
+		role: string;
+		company: string;
+		logoURL: string;
+		startDate: string;
+		endDate: string;
+		description: string;
+	}
+
+	let { role, company, logoURL, startDate, endDate, description }: Props = $props();
+</script>
+
+<div class="flex gap-10">
+	<a
+		href="/"
+		class="border-border-stroke-light flex aspect-square h-[80px] w-[80px] items-center justify-center rounded-lg border p-3"
+	>
+		<img src={logoURL} alt="logo" class=" block h-full w-full rounded-lg object-contain" />
+	</a>
+
+	<div class="flex flex-col gap-1.5">
+		<p class="text-primary-text text-lg font-semibold">{company}</p>
+		<p class="text-primary-text text-base">{role}</p>
+		<p class="text-subtext-text text-sm">{startDate} - {endDate}</p>
+		<p class="text-subtext-text mt-2.5 text-base">{description}</p>
+	</div>
+</div>
--- src/routes/experience/+page.svelte
+++ src/routes/experience/+page.svelte
@@ -1,3 +1,22 @@
-<section class="px-10 py-[120px]">
-	<div class="h-[400px] border">contribnution graph goes here</div>
+<script lang="ts">
+	import ExperienceCard from '$lib/components/ExperienceCard.svelte';
+	import { EXPERIENCES } from '$lib/constants';
+</script>
+
+<section class="flex flex-col gap-40 px-20 py-[120px]">
+	<div class="flex h-[400px] flex-col gap-14 border">
+		<p class="text-primary-text text-[40px] leading-[46px] tracking-[-1.8px]">Contribution Graph</p>
+
+		<div>contribution graph goes here...</div>
+	</div>
+
+	<div class="flex flex-col gap-20">
+		<p class="text-primary-text text-[40px] leading-[46px] tracking-[-1.8px]">Work Experience</p>
+
+		<div class="grid grid-cols-1 gap-24 md:grid-cols-2">
+			{#each EXPERIENCES as experience}
+				<ExperienceCard {...experience} />
+			{/each}
+		</div>
+	</div>
 </section>
