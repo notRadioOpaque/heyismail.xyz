95507a260bac93f63682b6a1dbf8eade1632620f
notRadioOpaque <ismailmuyideen.c05@gmail.com>
2025-04-08 18:55:51 +0100

    :art: redesigned About page layout with a split view, featuring a fixed left side and a scrollable right side for enhanced user experience

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,4 @@
-<h1>About page</h1>
+<div class="relative flex border">
+	<div class="flex h-screen w-1/2 items-center justify-center border">left side (fixed)</div>
+	<div class="flex w-1/2 items-center justify-center border">right side (scrolls)</div>
+</div>
