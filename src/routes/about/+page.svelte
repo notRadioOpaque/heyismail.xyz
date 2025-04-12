25b693b27f8aa151d05be5c7321d8c39010a2953
notRadioOpaque <ismailmuyideen.c05@gmail.com>
2025-04-12 01:59:29 +0100

    :art: redesigned About page layout to enhance user experience with improved structure, added buttons for resume viewing and downloading, and included a personal link for better engagement

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,4 +1,30 @@
-<div class="relative flex border">
-	<div class="flex h-screen w-1/2 items-center justify-center border">left side (fixed)</div>
+<div class="flex border">
+	<div class="h-screen w-1/2 border p-20">
+		<div class="flex w-[80%] flex-col items-center gap-4">
+			<div class="w-full overflow-hidden rounded-xl">
+				<img class="block h-full w-full object-cover" src="images/ismail.png" alt="" />
+			</div>
+
+			<div class="flex w-full justify-between gap-4">
+				<button
+					class="bg-btn-bg hover:text-primary-text hover:bg-btn-hover-bg flex w-full cursor-pointer items-center justify-center gap-2 rounded-md p-4 text-sm font-medium text-white transition-colors"
+					>View Resume <img class="w-5" src="./icons/link_icon.svg" alt="" /></button
+				>
+				<button
+					class="bg-btn-bg hover:text-primary-text hover:bg-btn-hover-bg flex w-max cursor-pointer items-center justify-center rounded-md p-4 text-sm font-medium transition-colors"
+					><img src="./icons/download_icon.svg" alt="" /></button
+				>
+			</div>
+
+			<div class="flex flex-col gap-4">
+				<a
+					href="/"
+					class="text-primary-text hover:border-secondary-bg border-b border-transparent text-[20px] leading-[20px] tracking-[-0.6px] transition-all hover:border-b"
+				>
+					hello<span class="text-tertiary-text">@</span>notradioopaque.dev
+				</a>
+			</div>
+		</div>
+	</div>
 	<div class="flex w-1/2 items-center justify-center border">right side (scrolls)</div>
 </div>
