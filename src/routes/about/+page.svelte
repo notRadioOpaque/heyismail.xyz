fc1870a31550a75566b08211e612325a40379402
notRadioOpaque <ismailmuyideen.c05@gmail.com>
2025-04-13 15:08:15 +0100

    :art: revamped About page layout by adjusting the width of sections, enhancing content with personal descriptions, and adding soft skills and creative arsenal sections for a more engaging user experience

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,5 @@
-<div class="flex border">
-	<div class="h-screen w-1/2 border p-20">
+<div class="flex">
+	<div class="h-screen w-1/3 p-20">
 		<div class="flex w-[80%] flex-col items-center gap-4">
 			<div class="w-full overflow-hidden rounded-xl">
 				<img class="block h-full w-full object-cover" src="images/ismail.png" alt="" />
@@ -26,5 +26,99 @@
 			</div>
 		</div>
 	</div>
-	<div class="flex w-1/2 items-center justify-center border">right side (scrolls)</div>
+
+	<div class="flex w-1/2 flex-col items-center p-20 pl-5">
+		<p class="text-primary-text text-[24px] leading-[33px] tracking-[-0.5px]">
+			Hi, I’m Andrew, a passionate web designer with a love for creating visually stunning
+			experiences. With a strong background in design and front-end development, I specialize in
+			crafting responsive websites that not only look great but also provide interactions across all
+			devices.
+		</p>
+
+		<div class="my-6 flex flex-col gap-4">
+			<p class="text-secondary-text text-[18px] leading-[28px] tracking-[-0.2px]">
+				Over the years, I’ve had the opportunity to work with a diverse range of clients, from
+				startups to established brands, helping them bring their visions to life online.
+			</p>
+
+			<p class="text-secondary-text text-[18px] leading-[28px] tracking-[-0.2px]">
+				I strongly believe in continuous learning and improving myself, so I try my best to learn in
+				any situation possible, unfavorable or not.
+			</p>
+
+			<p class="text-secondary-text text-[18px] leading-[28px] tracking-[-0.2px]">
+				Beyond learning, I enjoy writing technical articles and creating projects that both inspire
+				and benefit fellow developers. You can find my recent posts on freeCodeCamp and my personal
+				blog hosted on this site.
+			</p>
+		</div>
+
+		<div>quote goes here!</div>
+
+		<div class="my-6 flex flex-col gap-4">
+			<h3 class="text-[24px] leading-[33px] font-semibold tracking-[-1px]">Soft Skills</h3>
+			<p class="text-secondary-text text-[18px] leading-[28px] tracking-[-0.2px]">
+				Certain skills I've picked along the way that deserves mentioning:
+			</p>
+			<ul class="flex flex-col gap-4">
+				<li class="flex gap-3">
+					<div class="bg-secondary-bg mt-3 aspect-square h-[6px] w-[6px]"></div>
+					<div class="">
+						<span
+							class="text-primary-text text-[18px] leading-[28px] font-semibold tracking-[-0.4px]"
+							>Attention to detail</span
+						><span class="mr-1">:</span>
+						<span
+							class="text-secondary-text text-[18px] leading-[28px] tracking-[-0.2px] break-words whitespace-normal"
+						>
+							I take pleasure in creating designs and UIs with careful precision, emphasizing
+							quality over quantity.
+						</span>
+					</div>
+				</li>
+
+				<li class="flex gap-3">
+					<div class="bg-secondary-bg mt-3 aspect-square h-[6px] w-[6px]"></div>
+					<div class="">
+						<span
+							class="text-primary-text text-[18px] leading-[28px] font-semibold tracking-[-0.4px]"
+							>Open-source/Collaboration</span
+						><span class="mr-1">:</span>
+						<span
+							class="text-secondary-text text-[18px] leading-[28px] tracking-[-0.2px] break-words whitespace-normal"
+						>
+							I founded a community on GitHub called Communitypro that helps newbie developers learn
+							how to contribute to open-source projects and collaborate in building together.
+							Currently we are over 400 people strong with room for growth.
+						</span>
+					</div>
+				</li>
+
+				<li class="flex gap-3">
+					<div class="bg-secondary-bg mt-3 aspect-square h-[6px] w-[6px]"></div>
+					<div class="">
+						<span
+							class="text-primary-text text-[18px] leading-[28px] font-semibold tracking-[-0.4px]"
+							>Ability to work with little supervision</span
+						><span class="mr-1">:</span>
+						<span
+							class="text-secondary-text text-[18px] leading-[28px] tracking-[-0.2px] break-words whitespace-normal"
+						>
+							I understand people are busy and would love to get things done timely, so I do my best
+							in delivering assignments and projects with little supervision, sparing you the need
+							to closely manage/oversee my work.
+						</span>
+					</div>
+				</li>
+			</ul>
+		</div>
+
+		<div class="my-6 flex flex-col gap-4">
+			<h3 class="text-[24px] leading-[33px] font-semibold tracking-[-1px]">My Creative Arsenal</h3>
+			<p class="text-secondary-text text-[18px] leading-[28px] tracking-[-0.2px]">
+				Technologies, platforms, and gadgets I use regularly as a frontend developer — from React to
+				Twitch, from Figma to my trusty MacBook.
+			</p>
+		</div>
+	</div>
 </div>
