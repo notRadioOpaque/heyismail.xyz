6f57f060c7cd99404da9a8b1c4f40a22ca93c4e0
notRadioOpaque <ismailmuyideen.c05@gmail.com>
2025-04-16 10:40:37 +0100

    :sparkles: integrated GSAP animations in Socials component, adding scroll-triggered effects for title and description, enhancing visual engagement and user experience

--- src/routes/Socials.svelte
+++ src/routes/Socials.svelte
@@ -3,18 +3,47 @@
 	import GetInTouchCard from '$lib/components/GetInTouchCard.svelte';
 	import SocialCard from '$lib/components/SocialCard.svelte';
 	import { SOCIALS } from '$lib/constants';
+	import { gsap } from 'gsap';
+	import { ScrollTrigger } from 'gsap/ScrollTrigger';
+	import { onMount } from 'svelte';
 
 	const pageTitle = '{> Contact Me <}';
+
+	let section: any;
+	let title: HTMLDivElement;
+	let description: HTMLDivElement;
+
+	gsap.registerPlugin(ScrollTrigger);
+
+	onMount(() => {
+		const tl = gsap.timeline({
+			scrollTrigger: {
+				trigger: section,
+				start: '-50% top',
+				end: 'bottom center'
+			}
+		});
+
+		tl.from([title, description], {
+			y: 50,
+			opacity: 0,
+			duration: 0.5,
+			ease: 'power4.out'
+		});
+	});
 </script>
 
-<section class="flex flex-col gap-24 bg-[#f7f7f7] px-20 py-[160px]">
+<section bind:this={section} class="flex flex-col gap-24 bg-[#f7f7f7] px-20 py-[160px]">
 	<div class="flex flex-col gap-4">
-		<div class="flex items-center gap-2">
+		<div bind:this={title} class="flex items-center gap-2">
 			<Dot size="md" />
 			<p class="text-subtext-text text-lg font-medium">{@html pageTitle}</p>
 		</div>
 
-		<p class="text-primary-text w-[70%] text-[90px] leading-[99px] tracking-[-3.6px]">
+		<p
+			bind:this={description}
+			class="text-primary-text w-[70%] text-[90px] leading-[99px] tracking-[-3.6px]"
+		>
 			I’m all over the internet
 		</p>
 
