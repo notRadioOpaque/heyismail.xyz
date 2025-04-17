e2098260fcd302f762f0ea95f53340e99eed72d5
notRadioOpaque <ismailmuyideen.c05@gmail.com>
2025-04-17 15:01:39 +0100

    :sparkles: integrated GSAP animations in Projects page for enhanced visual engagement; added onMount lifecycle method to animate hero text and project blocks, improving user experience during page load.

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,6 +1,34 @@
 <script lang="ts">
 	import Button from '$lib/components/Button.svelte';
 	import Dot from '$lib/components/Dot.svelte';
+	import { gsap } from 'gsap';
+	import { onMount } from 'svelte';
+
+	let heroText: HTMLHeadingElement;
+	let subText: HTMLDivElement;
+	let block: HTMLDivElement;
+
+	onMount(() => {
+		const tl = gsap.timeline();
+
+		tl.from([heroText, subText], {
+			y: 100,
+			opacity: 0,
+			duration: 0.6,
+			ease: 'power4.out'
+		});
+
+		tl.from(
+			block,
+			{
+				y: 100,
+				opacity: 0,
+				duration: 0.6,
+				ease: 'power4.out'
+			},
+			'-=0.2'
+		);
+	});
 
 	let projectCount = [1, 2, 3];
 
@@ -11,19 +39,22 @@
 	};
 </script>
 
-<section class="p-[160px]">
+<section class="p-[100px]">
 	<div class="flex flex-col items-center justify-center gap-5">
-		<div class="flex items-center gap-2">
+		<div bind:this={subText} class="flex items-center gap-2">
 			<Dot size="md" />
 			<p class="text-subtext-text text-lg font-medium">My recent projects</p>
 		</div>
 
-		<p class="text-primary-text text-center text-[90px] leading-[99px] tracking-[-3.6px]">
+		<p
+			bind:this={heroText}
+			class="text-primary-text text-center text-[90px] leading-[99px] tracking-[-3.6px]"
+		>
 			Selected work
 		</p>
 	</div>
 
-	<div class="mt-20 flex flex-col gap-[120px]">
+	<div bind:this={block} class="mt-20 flex flex-col gap-[120px]">
 		{#each projectCount as project}
 			<a href={`/projects/${project}`} class="flex flex-col gap-8">
 				<div class="h-[683px] overflow-hidden rounded-2xl border border-gray-200 bg-gray-100">
