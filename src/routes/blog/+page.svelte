2751413ba194707e04deaa508351bc7a8f60065c
notRadioOpaque <ismailmuyideen.c05@gmail.com>
2025-04-17 15:42:18 +0100

    :sparkles: implemented GSAP animations for the blog page, enhancing visual engagement with animated hero text and subtext on page load; utilized onMount lifecycle method for improved user experience.

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,3 +1,37 @@
-<div class="flex h-[750px] w-full items-center justify-center">
-	<h1 class="text-[60px]">Coming soon! 🚀</h1>
+<script lang="ts">
+	import Dot from '$lib/components/Dot.svelte';
+	import { gsap } from 'gsap';
+	import { onMount } from 'svelte';
+
+	let heroText: HTMLHeadingElement;
+	let subText: HTMLDivElement;
+	let block: HTMLDivElement;
+
+	onMount(() => {
+		const tl = gsap.timeline();
+
+		tl.from([heroText, subText], {
+			y: 100,
+			opacity: 0,
+			duration: 0.6,
+			ease: 'power4.out'
+		});
+	});
+</script>
+
+<div class="flex h-[750px] w-full flex-col gap-28 px-20 py-[70px]">
+	<div class="flex flex-col items-center justify-center gap-5">
+		<div bind:this={subText} class="flex items-center gap-2">
+			<Dot size="md" />
+			<p class="text-subtext-text text-lg font-medium">Brain Dump</p>
+		</div>
+
+		<p
+			bind:this={heroText}
+			class="text-primary-text text-center text-[90px] leading-[99px] tracking-[-3.6px]"
+		>
+			Blogs
+		</p>
+	</div>
+	<h1 class="mx-auto text-[20px]">Coming soon! 🚀</h1>
 </div>
