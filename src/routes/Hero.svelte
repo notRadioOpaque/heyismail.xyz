3fee304fa0868efc1d7a0f0e3c92460e156b232e
notRadioOpaque <ismailmuyideen.c05@gmail.com>
2025-04-19 02:44:06 +0100

    :art: refined Hero section layout for improved responsiveness and visual appeal by adjusting padding, margins, and font sizes; optimized animation durations and removed unnecessary elements for cleaner presentation.

--- src/routes/Hero.svelte
+++ src/routes/Hero.svelte
@@ -40,7 +40,7 @@
 				duration: 0.6,
 				ease: 'power4.out'
 			},
-			'-=0.2'
+			'-=0.1'
 		);
 
 		tl.from(
@@ -48,14 +48,14 @@
 			{
 				x: 100,
 				opacity: 0,
-				duration: 0.6,
+				duration: 0.4,
 				ease: 'power4.out'
-			},
-			'-=0.2'
+			}
+			// '-=0.1'
 		);
 
 		tl.from(
-			[asideText, subText],
+			[subText],
 			{
 				y: 100,
 				opacity: 0,
@@ -67,10 +67,13 @@
 	});
 </script>
 
-<section class="flex overflow-x-hidden px-20 py-[120px]">
-	<div class="relative w-1/3">
-		<div class="mt-16 flex h-[150px] w-full items-center gap-4">
-			<div bind:this={imageDiv} class="opacity- relative z-20 w-[160px] overflow-hidden rounded-xl">
+<section class="flex flex-col overflow-x-hidden px-6 py-10 md:flex-row md:px-20 md:py-[120px]">
+	<div class="relative h-max w-full md:w-1/3">
+		<div class="flex h-full w-full items-center gap-4 md:mt-16">
+			<div
+				bind:this={imageDiv}
+				class="relative z-20 w-[100px] overflow-hidden rounded-xl md:w-[160px]"
+			>
 				<img class="block h-full w-full object-cover" src="images/ismail.png" alt="" />
 			</div>
 
@@ -154,29 +157,34 @@
 			</div>
 		</div>
 
-		<p
+		<!-- <p
 			bind:this={asideText}
-			class="absolute bottom-[195px] text-[20px] leading-[28px] font-medium tracking-[-0.8px]"
+			class="absolute text-[20px] leading-[28px] font-medium tracking-[-0.8px] md:bottom-[195px]"
 		>
 			(2023 - PRESENT)
-		</p>
+		</p> -->
 	</div>
 
-	<div class="flex w-2/3 flex-col gap-[70px]">
+	<div class="mt-12 flex w-full flex-col gap-[70px] md:mt-0 md:w-2/3">
 		<div class="flex flex-col gap-8">
 			<div bind:this={gimmick} class="flex items-center gap-2">
 				<Dot size="sm" />
 				<p class="text-primary-text text-sm">Available for work</p>
 			</div>
 
-			<h1 bind:this={heroText} class="text-[88px] leading-[92px] tracking-[-3.7px]">
+			<h1
+				bind:this={heroText}
+				class="text-[36px] leading-[45px] tracking-[-0.7px] md:text-[88px] md:leading-[92px] md:tracking-[-3.7px]"
+			>
 				Hi! I'm <span class="text-[#6b6b6b]">Ismail Muyideen</span>, a Front-end Engineer turning
 				your ideas into pixel-perfect realities
 			</h1>
 		</div>
 
 		<div bind:this={subText} class="flex flex-col gap-24">
-			<p class="text-2xl leading-[36px] font-medium tracking-[-1px] text-[#6b6b6b]">
+			<p
+				class="text-lg font-medium text-[#6b6b6b] md:text-2xl md:leading-[36px] md:tracking-[-1px]"
+			>
 				I’m dedicated to crafting websites that bring your ideas to life, combining design and
 				development to deliver fast, impactful results.
 			</p>
