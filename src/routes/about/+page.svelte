395fcb50279900d3ae38a9ebee94433592a53d30
notRadioOpaque <ismailmuyideen.c05@gmail.com>
2025-04-19 03:27:09 +0100

    :art: refined About section layout for improved responsiveness and visual appeal by adjusting font sizes, padding, and margins; optimized flexbox structure for better alignment of elements across different screen sizes.

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -42,14 +42,15 @@
 
 		<p
 			bind:this={heroText}
-			class="text-primary-text text-center text-[90px] leading-[99px] tracking-[-3.6px]"
+			class="mx-auto text-[36px] leading-[45px] tracking-[-0.7px] md:w-[70%] md:text-[88px]
+			md:leading-[92px] md:tracking-[-3.7px]"
 		>
 			About Me
 		</p>
 	</div>
 
-	<div bind:this={block} class="flex">
-		<div class="h-screen w-1/3 p-20 pt-0">
+	<div bind:this={block} class="flex flex-col gap-10 md:flex-row md:gap-0">
+		<div class="p-6 pt-0 md:h-screen md:w-1/3 md:p-20">
 			<div class="flex w-full flex-col items-center gap-4">
 				<div class="w-full overflow-hidden rounded-xl">
 					<img class="block h-full w-full object-cover" src="images/ismail.png" alt="" />
@@ -77,36 +78,42 @@
 			</div>
 		</div>
 
-		<div class="w-2/3 p-20 pt-0 pl-5">
-			<div class="w-4/5">
-				<p class="text-primary-text text-[24px] leading-[33px] tracking-[-0.5px]">
+		<div class="p-6 pt-0 md:w-2/3 md:p-20 md:pl-5">
+			<div class="w-full md:w-4/5">
+				<p
+					class="text-primary-text text-[20px] leading-[28px] tracking-[-0.2px] md:text-[24px] md:leading-[33px] md:tracking-[-0.5px]"
+				>
 					Hi, I’m Andrew, a passionate web designer with a love for creating visually stunning
 					experiences. With a strong background in design and front-end development, I specialize in
 					crafting responsive websites that not only look great but also provide interactions across
 					all devices.
 				</p>
 
-				<div class="my-6 flex flex-col gap-4">
-					<p class="text-secondary-text text-[18px] leading-[28px] tracking-[-0.2px]">
+				<div
+					class="text-secondary-text my-6 flex flex-col gap-4 text-base md:text-[18px] md:leading-[28px] md:tracking-[-0.2px]"
+				>
+					<p>
 						Over the years, I’ve had the opportunity to work with a diverse range of clients, from
 						startups to established brands, helping them bring their visions to life online.
 					</p>
 
-					<p class="text-secondary-text text-[18px] leading-[28px] tracking-[-0.2px]">
+					<p>
 						I strongly believe in continuous learning and improving myself, so I try my best to
 						learn in any situation possible, unfavorable or not.
 					</p>
 
-					<p class="text-secondary-text text-[18px] leading-[28px] tracking-[-0.2px]">
+					<p>
 						Beyond learning, I enjoy writing technical articles and creating projects that both
 						inspire and benefit fellow developers. You can find my recent posts on freeCodeCamp and
 						my personal blog hosted on this site.
 					</p>
 				</div>
 
-				<div class="mt-20 mb-12 flex flex-col gap-4">
+				<div class="mt-10 mb-12 flex flex-col gap-4 md:mt-20">
 					<h3 class="text-[24px] leading-[33px] font-semibold tracking-[-1px]">Soft Skills</h3>
-					<p class="text-secondary-text text-[18px] leading-[28px] tracking-[-0.2px]">
+					<p
+						class="text-secondary-text text-base md:text-[18px] md:leading-[28px] md:tracking-[-0.2px]"
+					>
 						Certain skills I've picked along the way that deserves mentioning:
 					</p>
 					<ul class="flex flex-col gap-4">
@@ -114,11 +121,11 @@
 							<div class="bg-secondary-bg mt-3 aspect-square h-[6px] w-[6px]"></div>
 							<div class="">
 								<span
-									class="text-primary-text text-[18px] leading-[28px] font-semibold tracking-[-0.4px]"
+									class="text-primary-text text-base font-semibold md:text-[18px] md:leading-[28px] md:tracking-[-0.2px]"
 									>Attention to detail</span
 								><span class="mr-1">:</span>
 								<span
-									class="text-secondary-text text-[18px] leading-[28px] tracking-[-0.2px] break-words whitespace-normal"
+									class="text-secondary-text text-base break-words whitespace-normal md:text-[18px] md:leading-[28px] md:tracking-[-0.2px]"
 								>
 									I take pleasure in creating designs and UIs with careful precision, emphasizing
 									quality over quantity.
@@ -130,11 +137,11 @@
 							<div class="bg-secondary-bg mt-3 aspect-square h-[6px] w-[6px]"></div>
 							<div class="">
 								<span
-									class="text-primary-text text-[18px] leading-[28px] font-semibold tracking-[-0.4px]"
+									class="text-primary-text text-base font-semibold md:text-[18px] md:leading-[28px] md:tracking-[-0.2px]"
 									>Open-source/Collaboration</span
 								><span class="mr-1">:</span>
 								<span
-									class="text-secondary-text text-[18px] leading-[28px] tracking-[-0.2px] break-words whitespace-normal"
+									class="text-secondary-text text-base break-words whitespace-normal md:text-[18px] md:leading-[28px] md:tracking-[-0.2px]"
 								>
 									I founded a community on GitHub called Communitypro that helps newbie developers
 									learn how to contribute to open-source projects and collaborate in building
@@ -147,11 +154,11 @@
 							<div class="bg-secondary-bg mt-3 aspect-square h-[6px] w-[6px]"></div>
 							<div class="">
 								<span
-									class="text-primary-text text-[18px] leading-[28px] font-semibold tracking-[-0.4px]"
+									class="text-primary-text text-base font-semibold md:text-[18px] md:leading-[28px] md:tracking-[-0.2px]"
 									>Ability to work with little supervision</span
 								><span class="mr-1">:</span>
 								<span
-									class="text-secondary-text text-[18px] leading-[28px] tracking-[-0.2px] break-words whitespace-normal"
+									class="text-secondary-text text-base break-words whitespace-normal md:text-[18px] md:leading-[28px] md:tracking-[-0.2px]"
 								>
 									I understand people are busy and would love to get things done timely, so I do my
 									best in delivering assignments and projects with little supervision, sparing you
@@ -166,7 +173,9 @@
 					<h3 class="text-[24px] leading-[33px] font-semibold tracking-[-1px]">
 						My Creative Arsenal
 					</h3>
-					<p class="text-secondary-text text-[18px] leading-[28px] tracking-[-0.2px]">
+					<p
+						class="text-secondary-text text-base md:text-[18px] md:leading-[28px] md:tracking-[-0.2px]"
+					>
 						Technologies, platforms, and gadgets I use regularly as a frontend developer — from
 						React to Twitch, from Figma to my trusty MacBook.
 					</p>
@@ -186,14 +195,14 @@
 									<div class="bg-tertiary-bg h-5 w-5"></div>
 									<a
 										href={url}
-										class="text-tertiary-text text-[18px] leading-[28px] tracking-[-0.4px] break-words whitespace-normal underline"
+										class="text-tertiary-text text-base break-words whitespace-normal underline md:text-[18px] md:leading-[28px] md:tracking-[-0.2px]"
 										>{name}</a
 									>
-									<p
-										class="text-secondary-text text-[18px] leading-[28px] tracking-[-0.2px] break-words whitespace-normal"
+									<!-- <p
+										class="text-secondary-text text-base break-words whitespace-normal md:text-[18px] md:leading-[28px] md:tracking-[-0.2px]"
 									>
 										- {description}
-									</p>
+									</p> -->
 								</li>
 							{/each}
 						</ul>
@@ -212,14 +221,14 @@
 									<div class="bg-tertiary-bg h-5 w-5"></div>
 									<a
 										href={url}
-										class="text-tertiary-text text-[18px] leading-[28px] tracking-[-0.4px] break-words whitespace-normal underline"
+										class="text-tertiary-text text-base break-words whitespace-normal underline md:text-[18px] md:leading-[28px] md:tracking-[-0.2px]"
 										>{name}</a
 									>
-									<p
-										class="text-secondary-text text-[18px] leading-[28px] tracking-[-0.2px] break-words whitespace-normal"
+									<!-- <p
+										class="text-secondary-text text-base break-words whitespace-normal md:text-[18px] md:leading-[28px] md:tracking-[-0.2px]"
 									>
 										- {description}
-									</p>
+									</p> -->
 								</li>
 							{/each}
 						</ul>
@@ -229,16 +238,18 @@
 
 			<div class="my-12 flex flex-col gap-4">
 				<h3 class="text-[24px] leading-[33px] font-semibold tracking-[-1px]">The Inspo List</h3>
-				<p class="text-secondary-text w-[90%] text-[18px] leading-[28px] tracking-[-0.2px]">
+				<p
+					class="text-secondary-text w-full text-base md:w-[90%] md:text-[18px] md:leading-[28px] md:tracking-[-0.2px]"
+				>
 					No cape, just code. These folks don’t just write software—they craft legacies. Each of
 					them has shaped how I think, build, and grow as a dev. No particular order, just massive
 					respect.
 				</p>
 
-				<div class="mt-8 flex gap-4">
+				<div class="mt-8 flex flex-col gap-4 md:flex-row">
 					<div
-						class={`grid grid-cols-2 gap-3 transition-all duration-300 ${
-							selected ? 'w-[600px]' : 'w-full grid-cols-3'
+						class={`order-2 grid gap-3 transition-all duration-300 md:grid-cols-2 ${
+							selected ? 'w-full md:w-[600px]' : 'w-full md:grid-cols-3'
 						}`}
 					>
 						{#each DEVELOPERS as dev}
@@ -261,7 +272,7 @@
 
 					{#if selected}
 						<div
-							class="flex w-full items-start justify-start rounded-lg p-4 transition-all duration-300"
+							class="flex w-full items-start justify-start rounded-lg p-4 px-0 transition-all duration-300 md:px-4"
 							data-view-transition-name="dev-panel"
 						>
 							<div class="space-y-2">
