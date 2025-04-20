67276f444d81d5a79b0a85a5f7580c25243b985d
notRadioOpaque <ismailmuyideen.c05@gmail.com>
2025-04-20 03:32:06 +0100

    closing in on the bug

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,8 @@
 	import Socials from './Socials.svelte';
 </script>
 
-<!-- <Hero />
+<!-- <Hero /> -->
 <Featured />
-<Socials /> -->
+<Socials />
 
 <p class="mt-10 text-center text-3xl">Just a test</p>
