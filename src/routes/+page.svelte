861787564494592d0c49f36b4be3b2c1ffc1a718
notRadioOpaque <ismailmuyideen.c05@gmail.com>
2025-04-20 03:10:03 +0100

    :art: removed commented-out imports for Testimonials and Toolbox components in +page.svelte to clean up the code and improve readability.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,8 @@
 	import Featured from './Featured.svelte';
 	import Hero from './Hero.svelte';
 	import Socials from './Socials.svelte';
-	// import Testimonials from './Testimonials.svelte';
-	// import Toolbox from './Toolbox.svelte';
 </script>
 
 <Hero />
 <Featured />
-<!-- <Toolbox /> -->
-<!-- <Testimonials /> -->
 <Socials />
