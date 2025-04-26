0014433ba2c97473a90dec7c7a69d504a9bbc804
notRadioOpaque <ismailmuyideen.c05@gmail.com>
2025-04-26 15:54:02 +0100

    :sparkles: Added Logo.svelte component for branding, featuring a customizable variant for light and dark themes. Includes a wave icon and brand name for improved visual identity.

---
+++ src/routes/Logo.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	let { variant }: { variant: 'light' | 'dark' } = $props();
+</script>
+
+<a href="/" class="flex w-max items-end gap-0">
+	<img src="/ghost_hand.svg" alt="Wave Icon" class="h-12 w-12" />
+	<span class={`text-base font-bold ${variant === 'light' ? '!text-white' : 'text-primary-text'}`}
+		>15MA1L</span
+	>
+</a>
