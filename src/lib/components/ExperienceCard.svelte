c6e70b59c064243322045427cdbe2182307f8158
notRadioOpaque <ismailmuyideen.c05@gmail.com>
2025-04-26 21:14:59 +0100

    :sparkles: Enhanced ExperienceCard component by integrating date-fns for improved date formatting. Updated date display to use formatted start and end dates, and adjusted icon size for better visual consistency.

--- src/lib/components/ExperienceCard.svelte
+++ src/lib/components/ExperienceCard.svelte
@@ -9,12 +9,19 @@
 	}
 
 	let { role, company, icon, startDate, endDate, description }: Props = $props();
+
+	import { parseISO, format } from 'date-fns';
+
+	const formatDate = (dateString: string): string => {
+		const parsedDate = parseISO(dateString);
+		return format(parsedDate, 'MMM dd, yyyy');
+	};
 </script>
 
 <div class="flex gap-3 xl:gap-10">
 	<a
 		href="/"
-		class="border-border-stroke-light flex aspect-square h-[80px] w-[80px] items-center justify-center rounded-lg border bg-[#3d8c2e] p-3"
+		class="border-border-stroke-light flex aspect-square h-[70px] w-[70px] items-center justify-center rounded-lg border bg-[#3d8c2e] p-3"
 	>
 		<img src={icon} alt="logo" class="block h-full w-full rounded-lg object-contain" />
 	</a>
@@ -22,7 +29,7 @@
 	<div class="flex flex-col gap-1.5">
 		<p class="text-primary-text text-lg font-semibold">{company}</p>
 		<p class="text-primary-text text-base">{role}</p>
-		<p class="text-subtext-text text-sm">{startDate} - {endDate}</p>
+		<p class="text-subtext-text text-sm">{formatDate(startDate)} - {formatDate(endDate)}</p>
 		<p class="text-subtext-text mt-2.5 text-sm xl:text-base">{description}</p>
 	</div>
 </div>
