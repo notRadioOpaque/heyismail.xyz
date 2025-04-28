4104b96ec592a15942ede56a19241fa0034f16ad
notRadioOpaque <ismailmuyideen.c05@gmail.com>
2025-04-28 19:36:29 +0100

    ✨ Enhanced README.md by adding a section for upcoming features and improving the overall structure for better user guidance. This update aims to provide clearer insights into future developments of the portfolio.

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script>
+	import SpecialBtn from '$lib/components/SpecialBtn.svelte';
+	import { goto } from '$app/navigation';
+</script>
+
+<div class="mt-[-110px] flex min-h-screen flex-col items-center justify-center">
+	<p class="text-[70px] tracking-[-2.7px]">Whoops! 🫠</p>
+
+	<p class="my-4 text-[18px] font-normal">you strayed!</p>
+
+	<div class="mt-10">
+		<SpecialBtn label="Find your way back 🏠" action={() => goto('/')}></SpecialBtn>
+	</div>
+</div>
